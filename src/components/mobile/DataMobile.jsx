e3f8c9c4db22fd9b1752c4cef399dce765fd3e16
DESKTOP-JQ1EBN4\Asus <ankeara017@gmail.com>
2025-05-06 16:31:35 +0700

    updated add new template and figma design

--- src/components/mobile/DataMobile.jsx
+++ src/components/mobile/DataMobile.jsx
@@ -1,92 +1,126 @@
-import Project1 from '../../assets/mockup/1.png';
-import Project2 from '../../assets/mockup/2.png';
-import Project3 from '../../assets/mockup/3.png';
-import Project4 from '../../assets/mockup/4.png';
-import Project5 from '../../assets/mockup/5.png';
-import Project6 from '../../assets/mockup/6.png';
-import Project7 from '../../assets/mockup/7.png';
-import Project8 from '../../assets/mockup/8.png';
-import Project9 from '../../assets/mockup/9.png';
-import Project10 from '../../assets/mockup/10.png';
-import Project11 from '../../assets/mockup/11.png';
-import Project12 from '../../assets/mockup/12.png';
-import Project27 from '../../assets/mockup/27.png';
-import Project30 from '../../assets/mockup/30.png';
-import Project31 from '../../assets/mockup/31.png';
-import Project32 from '../../assets/mockup/32.png';
-
+import Project1 from "../../assets/mockup/1.png";
+import Project2 from "../../assets/mockup/2.png";
+import Project3 from "../../assets/mockup/3.png";
+import Project4 from "../../assets/mockup/4.png";
+import Project5 from "../../assets/mockup/5.png";
+import Project6 from "../../assets/mockup/6.png";
+import Project7 from "../../assets/mockup/7.png";
+import Project8 from "../../assets/mockup/8.png";
+import Project9 from "../../assets/mockup/9.png";
+import Project10 from "../../assets/mockup/10.png";
+import Project11 from "../../assets/mockup/11.png";
+import Project12 from "../../assets/mockup/12.png";
+import Project27 from "../../assets/mockup/27.png";
+import Project30 from "../../assets/mockup/30.png";
+import Project31 from "../../assets/mockup/31.png";
+import Project32 from "../../assets/mockup/32.png";
+import Project41 from "../../assets/mockup/41.png";
+import Project42 from "../../assets/mockup/42.png";
+import Project43 from "../../assets/mockup/43.png";
+import Project46 from "../../assets/mockup/46.png";
 
 const DataMobile = [
   {
     id: 1,
-    image: Project1,
-    title: "--- Dark Shopping",
+    image: Project41,
+    title: "--- Travel",
     github: "#",
-    figma: "https://www.figma.com/design/S9lA7QSKhheb17NgB5UB8J/Fashion-Store?m=dev&t=17zxAoN5T3qJvSil-1",
+    figma:
+      "https://www.figma.com/design/ro0ZNylozEvpZZXtCnes41/Travel?t=XOhst19o4VxrguLg-0",
     website: "#",
   },
   {
     id: 2,
-    image: Project2,
-    title: "--- Fast Food",
+    image: Project42,
+    title: "--- Food & Grocery App!",
     github: "#",
-    figma: "https://www.figma.com/design/LnYOImVLnAF2Hx8NLLRkKQ/Hungry-Leap?m=dev&t=17zxAoN5T3qJvSil-1",
+    figma:
+      "https://www.figma.com/design/bJAfLYfi6u9JAjbtGDkF8N/Food---Grocery-App-?t=XOhst19o4VxrguLg-0",
     website: "#",
   },
   {
     id: 3,
-    image: Project3,
-    title: "--- Skincare Shop",
+    image: Project43,
+    title: "--- Storage iPhone",
     github: "#",
-    figma: "https://www.figma.com/design/nrzeLQ6EX8238gSF3Of0Bh/Skincare-App?m=dev&t=17zxAoN5T3qJvSil-1",
+    figma:
+      "https://www.figma.com/design/xg2zDrklCKcf0mPsyB8qHz/Storage-iPhone?t=XOhst19o4VxrguLg-0",
     website: "#",
-
   },
   {
     id: 4,
-    image: Project4,
-    title: "--- SpaceX Univers",
+    image: Project1,
+    title: "--- Dark Shopping",
     github: "#",
-    figma: "https://www.figma.com/design/ghy2ZOQKsyKt82GLn0Nnfl/Space-App?m=dev&t=qv3kFc7B0xt3cl0G-1",
+    figma:
+      "https://www.figma.com/design/S9lA7QSKhheb17NgB5UB8J/Fashion-Store?m=dev&t=17zxAoN5T3qJvSil-1",
     website: "#",
-
   },
   {
     id: 5,
-    image: Project5,
-    title: "--- Trending Alert",
+    image: Project2,
+    title: "--- Fast Food",
     github: "#",
-    figma: "https://www.figma.com/design/DKqvkUustb005aWQnXRYf8/Trend-Alter?node-id=0-1&m=dev&t=qv3kFc7B0xt3cl0G-1",
+    figma:
+      "https://www.figma.com/design/LnYOImVLnAF2Hx8NLLRkKQ/Hungry-Leap?m=dev&t=17zxAoN5T3qJvSil-1",
     website: "#",
-
   },
   {
     id: 6,
-    image: Project6,
-    title: "--- Cources Active",
+    image: Project3,
+    title: "--- Skincare Shop",
     github: "#",
-    figma: "https://www.figma.com/design/0ZfcrxYp1adCkvmu9kHOOG/Course-Activity?m=dev&t=qv3kFc7B0xt3cl0G-1",
+    figma:
+      "https://www.figma.com/design/nrzeLQ6EX8238gSF3Of0Bh/Skincare-App?m=dev&t=17zxAoN5T3qJvSil-1",
     website: "#",
-
   },
   {
     id: 7,
-    image: Project7,
-    title: "--- Sivance",
+    image: Project4,
+    title: "--- SpaceX Univers",
     github: "#",
-    figma: "https://www.figma.com/design/M0Uy31JV2jFoEzzgg8nvWr/Sivance?m=dev&t=qv3kFc7B0xt3cl0G-1",
+    figma:
+      "https://www.figma.com/design/ghy2ZOQKsyKt82GLn0Nnfl/Space-App?m=dev&t=qv3kFc7B0xt3cl0G-1",
     website: "#",
   },
   {
     id: 8,
-    image: Project8,
-    title: "--- E-Learning App",
-    github: "https://github.com/Ankeara/EducationApp_flutter/tree/my-educationapp",
-    figma: "#",
+    image: Project5,
+    title: "--- Trending Alert",
+    github: "#",
+    figma:
+      "https://www.figma.com/design/DKqvkUustb005aWQnXRYf8/Trend-Alter?node-id=0-1&m=dev&t=qv3kFc7B0xt3cl0G-1",
     website: "#",
   },
   {
     id: 9,
+    image: Project6,
+    title: "--- Cources Active",
+    github: "#",
+    figma:
+      "https://www.figma.com/design/0ZfcrxYp1adCkvmu9kHOOG/Course-Activity?m=dev&t=qv3kFc7B0xt3cl0G-1",
+    website: "#",
+  },
+  {
+    id: 10,
+    image: Project7,
+    title: "--- Sivance",
+    github: "#",
+    figma:
+      "https://www.figma.com/design/M0Uy31JV2jFoEzzgg8nvWr/Sivance?m=dev&t=qv3kFc7B0xt3cl0G-1",
+    website: "#",
+  },
+  {
+    id: 11,
+    image: Project8,
+    title: "--- E-Learning App",
+    github:
+      "https://github.com/Ankeara/EducationApp_flutter/tree/my-educationapp",
+    figma: "#",
+    website: "#",
+  },
+  {
+    id: 12,
     image: Project9,
     title: "--- Grocery App",
     github: "https://github.com/Ankeara/grocery_app",
@@ -94,7 +128,7 @@ const DataMobile = [
     website: "#",
   },
   {
-    id: 10,
+    id: 13,
     image: Project10,
     title: "--- IShop App",
     github: "https://github.com/Ankeara/homework2",
@@ -102,7 +136,7 @@ const DataMobile = [
     website: "#",
   },
   {
-    id: 11,
+    id: 14,
     image: Project11,
     title: "--- Helth Care App",
     github: "https://github.com/Ankeara/homework3",
@@ -110,48 +144,50 @@ const DataMobile = [
     website: "#",
   },
   {
-    id: 12,
+    id: 15,
     image: Project12,
     title: "--- Dictionary App",
-    github: "https://github.com/Ankeara/EducationApp_flutter/tree/my-educationapp",
+    github:
+      "https://github.com/Ankeara/EducationApp_flutter/tree/my-educationapp",
     figma: "#",
     website: "#",
   },
   {
-    id:27,
+    id: 16,
     image: Project27,
     title: "--- Exchange Money App",
-    github: "https://github.com/Ankeara/currency_converter_app_flutter/tree/my-currencyapp",
+    github:
+      "https://github.com/Ankeara/currency_converter_app_flutter/tree/my-currencyapp",
     figma: "#",
     website: "#",
   },
   {
-    id:30,
+    id: 17,
     image: Project30,
     title: "--- Organic Vagetables",
     github: "#",
-    figma: "https://www.figma.com/design/qGZBjOTppHdPU0GuEU8Lb7/Organic-Vagetables?node-id=0-1&m=dev&t=hKpM7lAc3So5kI4P-1",
+    figma:
+      "https://www.figma.com/design/qGZBjOTppHdPU0GuEU8Lb7/Organic-Vagetables?node-id=0-1&m=dev&t=hKpM7lAc3So5kI4P-1",
     website: "#",
   },
   {
-    id:31,
+    id: 18,
     image: Project31,
     title: "--- Cut For You",
     github: "#",
-    figma: "https://www.figma.com/design/fIOvd4sfOHU3g2iKKdfXKq/CutForYou?m=dev&t=hKpM7lAc3So5kI4P-1",
+    figma:
+      "https://www.figma.com/design/fIOvd4sfOHU3g2iKKdfXKq/CutForYou?m=dev&t=hKpM7lAc3So5kI4P-1",
     website: "#",
   },
   {
-    id:32,
+    id: 19,
     image: Project32,
     title: "--- Fast Food Deilivery",
     github: "#",
-    figma: "https://www.figma.com/design/ek6B7tgThLtTkXKHlTGzF0/FastFOod-delivery?m=dev&t=hKpM7lAc3So5kI4P-1",
+    figma:
+      "https://www.figma.com/design/ek6B7tgThLtTkXKHlTGzF0/FastFOod-delivery?m=dev&t=hKpM7lAc3So5kI4P-1",
     website: "#",
   },
 ];
 
-export default DataMobile
-
-
-
+export default DataMobile;
--- src/components/website/DataWebsite.jsx
+++ src/components/website/DataWebsite.jsx
@@ -1,25 +1,86 @@
-import Project13 from '../../assets/mockup/13.png';
-import Project14 from '../../assets/mockup/14.png';
-import Project15 from '../../assets/mockup/15.png';
-import Project16 from '../../assets/mockup/16.png';
-import Project17 from '../../assets/mockup/17.png';
-import Project18 from '../../assets/mockup/18.png';
-import Project19 from '../../assets/mockup/19.png';
-import Project20 from '../../assets/mockup/20.png';
-import Project21 from '../../assets/mockup/21.png';
-import Project22 from '../../assets/mockup/22.png';
-import Project23 from '../../assets/mockup/23.png';
-import Project24 from '../../assets/mockup/24.png';
-import Project25 from '../../assets/mockup/25.png';
-import Project26 from '../../assets/mockup/26.png';
-import Project28 from '../../assets/mockup/28.png';
-import Project29 from '../../assets/mockup/29.png';
-import Project33 from '../../assets/mockup/33.png';
-import Project34 from '../../assets/mockup/34.png';
-import Project35 from '../../assets/mockup/35.png';
-import Project36 from '../../assets/mockup/36.png';
+import Project13 from "../../assets/mockup/13.png";
+import Project14 from "../../assets/mockup/14.png";
+import Project15 from "../../assets/mockup/15.png";
+import Project16 from "../../assets/mockup/16.png";
+import Project17 from "../../assets/mockup/17.png";
+import Project18 from "../../assets/mockup/18.png";
+import Project19 from "../../assets/mockup/19.png";
+import Project20 from "../../assets/mockup/20.png";
+import Project21 from "../../assets/mockup/21.png";
+import Project22 from "../../assets/mockup/22.png";
+import Project23 from "../../assets/mockup/23.png";
+import Project24 from "../../assets/mockup/24.png";
+import Project25 from "../../assets/mockup/25.png";
+import Project26 from "../../assets/mockup/26.png";
+import Project28 from "../../assets/mockup/28.png";
+import Project29 from "../../assets/mockup/29.png";
+import Project33 from "../../assets/mockup/33.png";
+import Project34 from "../../assets/mockup/34.png";
+import Project35 from "../../assets/mockup/35.png";
+import Project36 from "../../assets/mockup/36.png";
+import Project37 from "../../assets/mockup/37.png";
+import Project38 from "../../assets/mockup/38.png";
+import Project39 from "../../assets/mockup/39.png";
+import Project40 from "../../assets/mockup/40.png";
+import Project44 from "../../assets/mockup/44.png";
+import Project45 from "../../assets/mockup/45.png";
 
 const DataWebsite = [
+  {
+    id: 1,
+    image: Project37,
+    title: "--- System Loans",
+    github: "https://github.com/Ankeara/system-loan-project",
+    figma:
+      "https://www.figma.com/design/Adfpwx2gltAFuJAmlzsfaw/System-loan?t=XOhst19o4VxrguLg-0",
+    website:
+      "https://ankeara.github.io/system-loan-project/src/pages/dashboard.html",
+  },
+  {
+    id: 2,
+    image: Project38,
+    title: "--- Coffee Website",
+    github: "#",
+    figma:
+      "https://www.figma.com/design/8MZZS3sgVwVKiKOUMqnk0g/Cofee-website?t=XOhst19o4VxrguLg-0",
+    website: "#",
+  },
+  {
+    id: 3,
+    image: Project39,
+    title: "--- login Template",
+    github: "#",
+    figma:
+      "https://www.figma.com/design/CJlcrZBOkkQdXBTkH6nL26/login?t=XOhst19o4VxrguLg-0",
+    website: "#",
+  },
+  {
+    id: 4,
+    image: Project40,
+    title: "---  building LS",
+    github: "",
+    figma:
+      "https://www.figma.com/design/P3lkUovajaGUgyS2OyzviJ/Buildingls?t=XOhst19o4VxrguLg-0",
+    website: "#",
+  },
+  {
+    id: 6,
+    image: Project44,
+    title: "--- POs system casier",
+    github: "#",
+    figma:
+      "https://www.figma.com/design/AZ4HaJjO0TsZeBl0lrYfOd/POs-system-casier?t=XOhst19o4VxrguLg-0",
+    website: "#",
+  },
+  {
+    id: 6,
+    image: Project45,
+    title: "--- Brightframe",
+    github: "#",
+    figma:
+      "https://www.figma.com/design/rda267fzjTuzTvnIRUm7hT/Brightframe?t=XOhst19o4VxrguLg-0",
+    website: "#",
+  },
   {
     id: 13,
     image: Project13,
@@ -29,7 +90,7 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:14,
+    id: 14,
     image: Project14,
     title: "--- Job Portal",
     github: "https://github.com/Ankeara/jobportal/tree/my-new-branch",
@@ -37,15 +98,15 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:15,
+    id: 15,
     image: Project15,
     title: "--- Dashboard Template",
     github: "https://github.com/Ankeara/litcoding_blog",
     figma: "#",
     website: "#",
   },
-   {
-    id:16,
+  {
+    id: 16,
     image: Project16,
     title: "--- Portfolio About Me",
     github: "https://github.com/Ankeara/portfolio_me",
@@ -53,10 +114,11 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:17,
+    id: 17,
     image: Project17,
     title: "--- CRUD Laravel Test",
-    github: "https://github.com/Ankeara/school_management_project/tree/system-school",
+    github:
+      "https://github.com/Ankeara/school_management_project/tree/system-school",
     figma: "#",
     website: "#",
   },
@@ -69,7 +131,7 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:19,
+    id: 19,
     image: Project19,
     title: "--- CRUD Image Larave",
     github: "https://github.com/Ankeara/CRUD_LARAVEL/tree/my-crud-branch",
@@ -77,15 +139,15 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:20,
+    id: 20,
     image: Project20,
     title: "--- Dashboard Login / Logout",
     github: "https://github.com/Ankeara/LoginLaravel/tree/login-laravel",
     figma: "#",
     website: "#",
   },
-   {
-    id:21,
+  {
+    id: 21,
     image: Project21,
     title: "--- Random Password Generator",
     github: "https://github.com/Ankeara/Password-Generator",
@@ -93,7 +155,7 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:22,
+    id: 22,
     image: Project22,
     title: "--- Age Calculator",
     github: "https://github.com/Ankeara/Age-Calculator",
@@ -101,7 +163,7 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:23,
+    id: 23,
     image: Project23,
     title: "--- Exchange Money",
     github: "https://github.com/Ankeara/Currency-Converter-App-in-JavaScript",
@@ -117,7 +179,7 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:25,
+    id: 25,
     image: Project25,
     title: "--- Generate QR Code",
     github: "https://github.com/Ankeara/QR-Code-Generator",
@@ -125,7 +187,7 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:26,
+    id: 26,
     image: Project26,
     title: "--- Random Emoji Generator",
     github: "https://github.com/Ankeara/Random-Emoji-Generator",
@@ -133,7 +195,7 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:28,
+    id: 28,
     image: Project28,
     title: "--- Library System",
     github: "https://github.com/Ankeara/Library_system/tree/my-library-system",
@@ -149,30 +211,34 @@ const DataWebsite = [
     website: "#",
   },
   {
-    id:36,
+    id: 36,
     image: Project36,
     title: "--- Short Cut To Use",
     github: "https://github.com/Ankeara/shortcut",
-    figma: "https://www.figma.com/design/mN3PK7MNWxPEQHVQwbQDQq/Shortcut-to-use?m=dev&t=hKpM7lAc3So5kI4P-1",
+    figma:
+      "https://www.figma.com/design/mN3PK7MNWxPEQHVQwbQDQq/Shortcut-to-use?m=dev&t=hKpM7lAc3So5kI4P-1",
     website: "https://shortcut-one.vercel.app/#",
   },
   {
-    id:34,
+    id: 34,
     image: Project34,
     title: "--- ArtFT",
     github: "#",
-    figma: "https://www.figma.com/design/GQ48CWZGKUMTUngn2ppBcs/ArtFT?m=dev&t=hKpM7lAc3So5kI4P-1",
+    figma:
+      "https://www.figma.com/design/GQ48CWZGKUMTUngn2ppBcs/ArtFT?m=dev&t=hKpM7lAc3So5kI4P-1",
     website: "#",
   },
   {
-    id:35,
+    id: 35,
     image: Project35,
     title: "--- BD Tournament",
     github: "#",
-    figma: "https://www.figma.com/design/txlsTUJiiwiqyPOf1n2ULe/BD-Tournament?m=dev&t=hKpM7lAc3So5kI4P-1",
+    figma:
+      "https://www.figma.com/design/txlsTUJiiwiqyPOf1n2ULe/BD-Tournament?m=dev&t=hKpM7lAc3So5kI4P-1",
     website: "#",
-  },{
-    id:33,
+  },
+  {
+    id: 33,
     image: Project33,
     title: "--- Web Link",
     github: "https://github.com/Ankeara/web_link",
@@ -181,7 +247,4 @@ const DataWebsite = [
   },
 ];
 
-export default DataWebsite
-
-
-
+export default DataWebsite;
